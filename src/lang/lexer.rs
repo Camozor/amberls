02a166de664e0cb12772e0ee01a6fcc227732e77
Camille Vauchel <camille.vauchel@neomi.immo>
2025-01-11 15:01:19 +0100

    Failing test

--- src/lang/lexer.rs
+++ src/lang/lexer.rs
@@ -2,12 +2,14 @@ use super::token::{Token, TokenType};
 
 pub struct Lexer {
     pub input: String,
+    pub position: i32,
 }
 
 impl Lexer {
     pub fn new(input: &str) -> Self {
         Lexer {
             input: String::from(input),
+            position: 0,
         }
     }
 
@@ -29,7 +31,37 @@ mod tests {
 
         let lexer = Lexer::new(input);
 
-        let token = lexer.next_token();
-        assert_eq!(token.token_type, TokenType::ASSIGN)
+        let expected_tokens = vec![
+            Token {
+                token_type: TokenType::ASSIGN,
+                literal: String::from("="),
+            },
+            Token {
+                token_type: TokenType::PLUS,
+                literal: String::from("+"),
+            },
+            Token {
+                token_type: TokenType::LPAREN,
+                literal: String::from("("),
+            },
+            Token {
+                token_type: TokenType::RPAREN,
+                literal: String::from(")"),
+            },
+            Token {
+                token_type: TokenType::LBRACE,
+                literal: String::from("{"),
+            },
+            Token {
+                token_type: TokenType::RBRACE,
+                literal: String::from("}"),
+            },
+        ];
+
+        for expected_token in expected_tokens {
+            let token = lexer.next_token();
+            assert_eq!(token.token_type, expected_token.token_type);
+            assert_eq!(token.literal, expected_token.literal);
+        }
     }
 }
